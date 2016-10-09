29ce89f14d3c75f0d4dd1262bc346a8228d6be4a
Martin Bárta <martin.barta@hotmail.com>
2016-10-09 19:42:09 +0200

    Adds comment to Linux deploy script.
    Former-commit-id: 881275c124197db9446effdc635e2d1ea2cd6be5
    Former-commit-id: c724de94958fa85901551a58f3d9bf89439a1d70

--- deploy_unix.sh
+++ deploy_unix.sh
@@ -10,8 +10,12 @@ echo Deploying to $folder
 
 mkdir -p $folder
 
+# Assume that the CD is the root of the project
+# and that there are two symbolic links to the executables.
+
 cp App/App $folder
 cp App/App.debug $folder/App.debug
+
 cp App/*.vert $folder
 cp App/*.frag $folder
 cp App/*.cl $folder
