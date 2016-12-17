953ae51f633682023e4d73763930428fb3c03cf3
Martin Bárta <martin.barta@hotmail.com>
2016-12-17 20:26:08 +0100

    Fix Unix build.
    Former-commit-id: 5caf057ebc1d8100bdd4c32518b2ea3ac11ef78d
    Former-commit-id: 72bd9daa8c85cfcf4df0817a337418a23f57f743

--- deploy_unix.sh
+++ deploy_unix.sh
@@ -2,29 +2,29 @@
 
 # Usage: ./deploy_unix.sh fileName
 #
+# This script make a ZIP archive of the Linux version of the program.
 # Output is fileName.zip in the current directory.
+# Build and package the 32-bit version separately.
 
 name=$1
 if [ "$name" == "" ]
 then
-name=ZSBS-Linux
+	name=ZSBS-Linux
 fi
 
 folder=`mktemp -d -p .`
 
-echo -e Deploying to $folder'\n'
+echo -e Deploying to $name'\n'
 
 mkdir -p $folder/$name
 
-cp -v `find .. -type f -name App | grep 64 | grep Release` $folder/$name/App
-cp -v `find .. -type f -name App | grep 64 | grep Debug` $folder/$name/App.debug
-
-cp -v `find .. -type f -name App | grep 32 | grep Release` $folder/$name/App
-cp -v `find .. -type f -name App | grep 32 | grep Debug` $folder/$name/App.debug
+cp -v `find .. -type f -name App | grep ZSBS | grep Release` $folder/$name/App
+cp -v `find .. -type f -name App | grep ZSBS | grep Debug` $folder/$name/App.debug
 
 cd $folder
 zip -r $name.zip $name
 mv $name.zip ..
 cd -
+
 rm -r $folder
 
