22a4e272665a90318bf55f524689e343df1e96ed
Martin Bárta <martin.barta@hotmail.com>
2016-12-17 19:39:54 +0100

    Fix Windows build.
    Former-commit-id: ee0b2d79868a4c54c19b0bfa455c343ad7654573
    Former-commit-id: 75dd88d45b0b9d1bbaa494bb7d84256b778185af

--- deploy_win.sh
+++ deploy_win.sh
@@ -2,26 +2,43 @@
 
 # Usage: ./deploy_unix.sh fileName
 #
-# Output is fileName.zip in the current directory.
+# This script make a ZIP archive of the Linux version of the program.
+# Output is fileName.zip and fileName-32.zip in the current directory.
 # Use Git Bash or a similar tool to run this.
 
 name=$1
 if [ "$name" == "" ]
 then
-name=ZSBS-Windows
+    name=ZSBS-Windows
 fi
 
 folder=`mktemp -d -p .`
+folder32=`mktemp -d -p .`
 
-echo -e Deploying to $name.zip'\n'
+echo -e Deploying to $name.zip and $name-32.zip'\n'
 
-mkdir -p $folder/$name
+mkdir -p $folder/$name/platforms
+mkdir -p $folder32/$name-32/platforms
 
-cp -v `find .. -name App.exe | grep 64 | grep Release` $folder/$name/App.exe
-cp -v `find .. -name App.exe | grep 32 | grep Release` $folder/$name/App-32.exe
+cp -v `find .. -name App.exe | grep ZSBS | grep 64 | grep Release` $folder/$name/App.exe
+cp -v `find .. -name App.exe | grep ZSBS | grep 32 | grep Release` $folder32/$name-32/App.exe
+
+QT_DIR=C:/Qt/5.7/msvc2015_64
+cp -v $QT_DIR/bin/Qt5Core.dll $folder/$name
+cp -v $QT_DIR/bin/Qt5Gui.dll $folder/$name
+cp -v $QT_DIR/bin/Qt5Widgets.dll $folder/$name
+cp -v $QT_DIR/plugins/platforms/qwindows.dll $folder/$name/platforms
+
+QT_DIR=C:/Qt/5.7/msvc2015
+cp -v $QT_DIR/bin/Qt5Core.dll $folder32/$name-32
+cp -v $QT_DIR/bin/Qt5Gui.dll $folder32/$name-32
+cp -v $QT_DIR/bin/Qt5Widgets.dll $folder32/$name-32
+cp -v $QT_DIR/plugins/platforms/qwindows.dll $folder32/$name-32/platforms
 
 #zip -r $name.zip $name
+rm -f "$name.zip" "$name-32.zip"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('$folder', '$name.zip'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('$folder32', '$name-32.zip'); }"
 
-rm -r $folder
+rm -r $folder $folder32
 
