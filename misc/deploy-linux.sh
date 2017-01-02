caf2af3a4d02da8f7522f16d99af097738ed3215
Martin Bárta <martin.barta@hotmail.com>
2017-01-02 18:16:37 +0100

    Add readme to packages.

--- misc/deploy-linux.sh
+++ misc/deploy-linux.sh
@@ -3,7 +3,8 @@
 # Usage: ./misc/deploy-linux.sh fileName
 #
 # This script makes a standalone ZIP package for distribution on Linux.
-# Tested on Ubuntu 14/16.
+#
+# Tested on Ubuntu 14/16, Debian 8.
 # You must build and package the 32-bit version separately (possibly on a different OS).
 
 name=$1
@@ -19,8 +20,8 @@ echo -e Deploying to $name'\n'
 mkdir -p $folder/$name/platforms
 mkdir -p $folder/$name/xcbglintegrations
 
-cp -v `find .. -type f -name Alenka | grep Alenka | grep Release` $folder/$name/Alenka
-cp -v `find .. -type f -name Alenka | grep Alenka | grep Debug` $folder/$name/Alenka.debug
+cp -v `find .. -type f -name Alenka | grep Alenka | grep Release | grep 5.7` $folder/$name/Alenka
+cp -v `find .. -type f -name Alenka | grep Alenka | grep Debug | grep 5.7` $folder/$name/Alenka.debug
 
 PLUGIN=/opt/Qt/5.7/gcc_64/plugins
 xcbglintegrations
@@ -32,12 +33,17 @@ cp -v `ldd $folder/$name/Alenka $PLUGIN/platforms/libqxcb.so | grep -i qt | awk 
 echo '#!/bin/sh
 
 DIR=`dirname $0`
-export LD_LIBRARY_PATH=$DIR:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DIR:$AMDAPPSDKROOT/lib/x86_64/sdk:$LD_LIBRARY_PATH
 export LC_NUMERIC=en_US.UTF-8
 ' | tee $folder/$name/runAlenka > $folder/$name/runAlenka.debug
 echo '$DIR/Alenka "$@"' >> $folder/$name/runAlenka
 echo '$DIR/Alenka.debug "$@"' >> $folder/$name/runAlenka.debug
 chmod u+x $folder/$name/run*
+
+echo 'This is a standalone package.
+
+Use "./runAlenka" to launch the program.
+' > $folder/$name/README
 
 cd $folder
 zip -r $name.zip $name
@@ -46,4 +52,3 @@ cd -
 
 rm -r $folder
 
-#TODO: make a README with installation instructions
--- misc/deploy-ubu.sh
+++ misc/deploy-ubu.sh
@@ -2,7 +2,8 @@
 
 # Usage: ./misc/deploy-ubu.sh fileName
 #
-# This script makes a ZIP package for distribution on one version of OS.
+# Use this script to make a ZIP package for Ubuntu 16 that has preinstalled Qt 5.5.
+#
 # You must build and package the 32-bit version separately (possibly on a different OS).
 
 name=$1
@@ -17,17 +18,25 @@ echo -e Deploying to $name'\n'
 
 mkdir -p $folder/$name
 
-cp -v `find .. -type f -name Alenka | grep Alenka | grep Release` $folder/$name/Alenka
-cp -v `find .. -type f -name Alenka | grep Alenka | grep Debug` $folder/$name/Alenka.debug
+cp -v `find .. -type f -name Alenka | grep Alenka | grep Release | grep 5.5` $folder/$name/Alenka
+cp -v `find .. -type f -name Alenka | grep Alenka | grep Debug | grep 5.5` $folder/$name/Alenka.debug
 
 echo '#!/bin/sh
 
 DIR=`dirname $0`
+export LD_LIBRARY_PATH=$DIR:$AMDAPPSDKROOT/lib/x86_64/sdk:$LD_LIBRARY_PATH
 export LC_NUMERIC=en_US.UTF-8
 ' | tee $folder/$name/runAlenka > $folder/$name/runAlenka.debug
 echo '$DIR/Alenka "$@"' >> $folder/$name/runAlenka
 echo '$DIR/Alenka.debug "$@"' >> $folder/$name/runAlenka.debug
 chmod u+x $folder/$name/run*
+
+echo 'This version requires Qt 5.5 shared libraries to be installed.
+For Ubuntu 16 you can do that by installing appropriate packages (usualy they are already installed).
+For Ubuntu 14 use the installer from Qt website to install package "Qt 5.5 -- Desktop gcc".
+
+Use "./runAlenka" to launch the program.
+' > $folder/$name/README
 
 cd $folder
 zip -r $name.zip $name
