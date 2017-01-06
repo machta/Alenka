3335ac682ff1e69a8b624f64f40837b9c014ea78
Martin Bárta <martin.barta@hotmail.com>
2017-01-06 14:08:37 +0100

    Fix a script.

--- misc/make-install.sh
+++ misc/make-install.sh
@@ -6,20 +6,21 @@
 # Use this when you make changes there and then want to build the main app.
 # Use Git Bash or a similar tool to run this on Windows.
 
+export MAKEFLAGS=-j4 # Change this to controll how many parallel jobs make can use.
+
 cd Alenka-File
 for build in `ls build* -d`
 do
 	cd $build
 	if [ `echo $build | grep Release` ]
 	then
-		cmake --build . --config Release --target install-alenka-file &
+		cmake --build . --config Release --target install-alenka-file
 	else
-		cmake --build . --target install-alenka-file &
+		cmake --build . --target install-alenka-file
 	fi
 	cd -
 done
 cd ..
-wait
 
 cd Alenka-Signal
 for build in `ls build* -d`
@@ -27,12 +28,10 @@ do
 	cd $build
 	if [ `echo $build | grep Release` ]
 	then
-		cmake --build . --config Release --target install-alenka-signal &
+		cmake --build . --config Release --target install-alenka-signal
 	else
-		cmake --build . --target install-alenka-signal &
+		cmake --build . --target install-alenka-signal
 	fi
 	cd -
 done
 cd ..
-wait
-
