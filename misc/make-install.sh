36eab3f9fd23047d6d0fa0ed0c56286d9768a1d6
Martin Bárta <martin.barta@hotmail.com>
2017-01-08 20:35:57 +0100

    Add summary to make-install.sh.

--- misc/make-install.sh
+++ misc/make-install.sh
@@ -11,27 +11,39 @@ export MAKEFLAGS=-j4 # Change this to controll how many parallel jobs make can u
 cd Alenka-File
 for build in `ls build* -d`
 do
+	echo Building Alenka-File/$build
 	cd $build
 	if [ `echo $build | grep Release` ]
 	then
-		cmake --build . --config Release --target install-alenka-file
+		cmake --build . --config Release --target install-alenka-file && STATUS=OK || STATUS=fail
 	else
-		cmake --build . --target install-alenka-file
+		cmake --build . --target install-alenka-file && STATUS=OK || STATUS=fail
 	fi
 	cd -
+	SUMMARY=$SUMMARY`printf '%-40s %s' Alenka-File/$build $STATUS`'\n'
+	echo
 done
 cd ..
 
 cd Alenka-Signal
 for build in `ls build* -d`
 do
+	echo Building Alenka-Signal/$build
 	cd $build
 	if [ `echo $build | grep Release` ]
 	then
-		cmake --build . --config Release --target install-alenka-signal
+		cmake --build . --config Release --target install-alenka-signal && STATUS=OK || STATUS=fail
 	else
-		cmake --build . --target install-alenka-signal
+		cmake --build . --target install-alenka-signal && STATUS=OK || STATUS=fail
 	fi
 	cd -
+	SUMMARY=$SUMMARY`printf '%-40s %s' Alenka-Signal/$build $STATUS`'\n'
+	echo
 done
 cd ..
+
+echo
+echo ================= Build summary =================
+echo "Library                                  Status"
+echo =================================================
+echo -en "$SUMMARY"
