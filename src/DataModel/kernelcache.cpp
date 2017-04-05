509cdc56bff589974a0fba5c32be7647bf860fe8
Martin Bárta <martin.barta@hotmail.com>
2017-04-05 11:50:39 +0200

    Switch to binary kernel cache format.

--- src/DataModel/kernelcache.cpp
+++ src/DataModel/kernelcache.cpp
@@ -3,13 +3,13 @@
 #include "../error.h"
 #include "../options.h"
 
-#include <pugixml.hpp>
-
 #include <QDir>
 #include <QCoreApplication>
 
+#include <fstream>
+#include <sstream>
+
 using namespace std;
-using namespace pugi;
 
 namespace
 {
@@ -23,7 +23,7 @@ string cacheFilePath()
 	if (str.empty())
 		str = QCoreApplication::applicationDirPath().toStdString();
 
-	return str + QDir::separator().toLatin1() + "kernel-cache-" + to_string(platform) + '-' + to_string(device) + ".xml";
+	return str + QDir::separator().toLatin1() + "kernel-cache-" + to_string(platform) + '-' + to_string(device) + ".txt";
 }
 
 } // namespace
@@ -33,65 +33,71 @@ KernelCache::KernelCache()
 	// TODO: Perhaps switch to byte-size based capacity system.
 	cache.setMaxCost(PROGRAM_OPTIONS["kernelCacheSize"].as<int>());
 
-	// Load  from a file.
-	xml_document xml;
+	// Load.
 	string filePath = cacheFilePath();
-	if (!xml.load_file(filePath.c_str()))
+	ifstream file(filePath);
+
+	if (file.is_open())
+	{
+		int size;
+		file >> size;
+
+		for (int i = 0; i < size; ++i)
+		{
+			size_t codeSize;
+			file >> codeSize;
+			file.ignore(2, '\n');
+
+			unique_ptr<char[]> code(new char[codeSize + 1]{0});
+			file.read(code.get(), codeSize);
+
+			size_t binarySize;
+			file >> binarySize;
+			file.ignore(2, '\n');
+
+			auto binary = new vector<unsigned char>(binarySize);
+			file.read(reinterpret_cast<char*>(binary->data()), binarySize);
+
+			cache.insert(QString(code.get()), binary);
+		}
+	}
+	else
 	{
 		logToFileAndConsole("Failed to open kernel cache at " << filePath << ".");
-	}
-
-	xml_node node = xml.child("cache");
-	int size = node.attribute("size").as_int();
-
-	if (size <= 0)
-		return;
-
-	xml_node item = node.child("item");
-	while (item)
-	{
-		QString code = QString::fromStdString(item.child("code").text().as_string());
-		string binary = item.child("binary").text().as_string();
-
-		if (code.isEmpty() || binary.empty())
-			continue;
-
-		auto data = new vector<unsigned char>();
-		for (char c : binary)
-			data->push_back(c);
-
-		cache.insert(code, data);
-
-		item = item.next_sibling("item");
 	}
 }
 
 KernelCache::~KernelCache()
 {
-	// Save to a file.
+	// Save.
 	int size = cache.size();
 	if (size <= 0)
 		return;
 
-	xml_document xml;
-
-	xml_node node = xml.append_child("cache");
-	node.append_attribute("size").set_value(size);
-
-	const auto& keys = cache.keys();
-	for (auto e : keys)
-	{
-		xml_node item = node.append_child("item");
-		item.append_child("code").append_child(node_pcdata).set_value(e.toStdString().c_str());
-
-		std::vector<unsigned char> binary(*find(e));
-		binary.push_back(0);
-
-		item.append_child("binary").append_child(node_pcdata).set_value(reinterpret_cast<char*>(binary.data()));
-	}
-
 	string filePath = cacheFilePath();
-	if (!xml.save_file(filePath.c_str()))
+	ofstream file(filePath);
+
+	if (file.is_open())
+	{
+		file << size << endl;
+		const auto& keys = cache.keys();
+
+		for (auto qCode : keys)
+		{
+			size_t codeSize = qCode.size();
+			string code = qCode.toStdString();
+
+			file << codeSize << endl;
+			file.write(code.data(), codeSize) << endl;
+
+			auto binary = find(qCode);
+			size_t binarySize = binary->size();
+
+			file << binarySize << endl;
+			file.write(reinterpret_cast<const char*>(binary->data()), binarySize) << endl;
+		}
+	}
+	else
 	{
 		logToFileAndConsole("Failed to save kernel cache to " << filePath << ".");
 	}
